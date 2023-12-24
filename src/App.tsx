4003a67d51176219f134bd28dc19b05c7bc0bfd7
Joseph Mumford <josephamumford@gmail.com>
2023-12-24 17:23:08 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,87 +7,9 @@ import Header from "./components/Header";
 import Login from "./components/Login";
 import SideBar from "./components/SideBar";
 
-const socket = new WebSocket("ws://localhost:8080");
+const socket = new WebSocket(`ws://${import.meta.env.VITE_HOST}:8080`);
+console.log(`ws://${import.meta.env.VITE_HOST}:8080`);
 
-const testMessages: Message[] = [
-  {
-    content: "Hello",
-    id: "100",
-    participantId: "user 1",
-    timestamp: "1",
-    type: "MESSAGE",
-  },
-  {
-    content: "Hi",
-    id: "101",
-    participantId: "user 2",
-    timestamp: "2",
-    type: "MESSAGE",
-  },
-  {
-    content: "What are you doing?",
-    id: "102",
-    participantId: "user 1",
-    timestamp: "3",
-    type: "MESSAGE",
-  },
-  {
-    content: "Nothing much",
-    id: "103",
-    participantId: "user 2",
-    timestamp: "4",
-    type: "MESSAGE",
-  },
-  {
-    content: "Cool",
-    id: "104",
-    participantId: "user 1",
-    timestamp: "5",
-    type: "MESSAGE",
-  },
-  {
-    content: "Cool",
-    id: "105",
-    participantId: "user 1",
-    timestamp: "5",
-    type: "MESSAGE",
-  },
-  {
-    content: "Cool",
-    id: "106",
-    participantId: "user 1",
-    timestamp: "5",
-    type: "MESSAGE",
-  },
-  {
-    content: "Cool",
-    id: "107",
-    participantId: "user 1",
-    timestamp: "5",
-    type: "MESSAGE",
-  },
-  {
-    content: "Cool",
-    id: "108",
-    participantId: "user 1",
-    timestamp: "5",
-    type: "MESSAGE",
-  },
-  {
-    content: "Cool",
-    id: "109",
-    participantId: "user 1",
-    timestamp: "5",
-    type: "MESSAGE",
-  },
-  {
-    content: "Cool",
-    id: "110",
-    participantId: "user 1",
-    timestamp: "5",
-    type: "MESSAGE",
-  },
-];
 
 function App() {
   const [displayName, setDisplayName] = useState<string>("");
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST,
+      port: 5173,
+    },
+  };
 });
